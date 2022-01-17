a23f0483163e721ae388e183291ae04aca7bea07
Zenky <bzenky@gmail.com>
2022-01-17 19:40:23 -0300

    feat: folder for desktop/mobile - refactor: divide desktop/mobile in its own async function

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
 const puppeteer = require('puppeteer')
 const mobile = puppeteer.devices['iPhone X']
-
-const exampleSites = [
-    {
-        name: 'Mercado Livre',
-        url: 'http://mercadolivre.com.br',
-    },
-    {
-        name: 'Amazon AWS',
-        url: 'https://aws.amazon.com/',
-    },
-    {
-        name: 'Vercel',
-        url: 'http://vercel.com',
-    },
-]
+const { exampleSites } = require('./src/exampleSites')
+const fs = require('fs-extra')
 
 function wait(ms) {
     return new Promise(resolve => setTimeout(() => resolve(), ms))
@@ -26,7 +13,13 @@ function wait(ms) {
 
     for (let i = 0; i < exampleSites.length; i++) {
 
-        await page.setViewport({ width: 1920, height: 1080, setDeviceScaleFactor: 0.75 })
+        const dir = `./screenshot/${exampleSites[i].name}`
+
+        fs.ensureDirSync(dir);
+        fs.ensureDirSync(`${dir}/desktop`);
+        fs.ensureDirSync(`${dir}/mobile`);
+
+        await page.setViewport({ width: 1920, height: 1080 })
         await page.goto(exampleSites[i].url, { waitUntil: 'load' })
 
         const bodyHandle = await page.$('body')
@@ -49,11 +42,51 @@ function wait(ms) {
 
         await wait(200)
 
-        await page.screenshot({ path: `./screenshot/${exampleSites[i].name}-desktop.jpeg`, quality: 20, fullPage: true })
+        await page.screenshot({ path: `./screenshot/${exampleSites[i].name}/desktop/lp.jpeg`, quality: 20, fullPage: true })
 
-        await page.emulate(mobile)
-        await page.screenshot({ path: `./screenshot/${exampleSites[i].name}-mobile.jpeg`, quality: 20, fullPage: true })
+        console.log(`Site (desktop): ${i + 1} de ${exampleSites.length} capturados com sucesso!`)
     }
 
     await browser.close()
+
+})();
+
+(async () => {
+    const browser = await puppeteer.launch({ defaultViewport: null })
+    const page = await browser.newPage()
+
+    for (let j = 0; j < exampleSites.length; j++) {
+
+        await page.setViewport({ width: 1920, height: 1080 })
+        await page.emulate(mobile)
+        await page.setViewport({ width: 375, height: 812 })
+        await page.goto(exampleSites[j].url, { waitUntil: 'load' })
+
+        const bodyHandle = await page.$('body')
+        const { height } = await bodyHandle.boundingBox()
+        await bodyHandle.dispose()
+
+        const viewportHeight = page.viewport().height
+        let viewportIncr = 0
+        while (viewportIncr + viewportHeight < height) {
+            await page.evaluate(_viewportHeight => {
+                window.scrollBy(0, _viewportHeight)
+            }, viewportHeight)
+            await wait(100)
+            viewportIncr = viewportIncr + viewportHeight
+        }
+
+        await page.evaluate(_ => {
+            window.scrollTo(0, 0)
+        });
+
+        await wait(200)
+
+        await page.screenshot({ path: `./screenshot/${exampleSites[j].name}/mobile/lp.jpeg`, quality: 20, fullPage: true })
+
+        console.log(`Site (mobile): ${j + 1} de ${exampleSites.length} capturados com sucesso!`)
+    }
+
+    await browser.close()
+
 })()
---
+++ src/exampleSites.js
@@ -0,0 +1,16 @@
+const exampleSites = [
+    {
+        name: 'Mercado Livre',
+        url: 'http://mercadolivre.com.br',
+    },
+    {
+        name: 'Amazon AWS',
+        url: 'https://aws.amazon.com/',
+    },
+    {
+        name: 'Vercel',
+        url: 'http://vercel.com',
+    },
+]
+
+module.exports = { exampleSites }
